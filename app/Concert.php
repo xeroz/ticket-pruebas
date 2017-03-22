0c503d5053836c613a006332a43b281b73d4bd65
juan <chavezvasquezjuan@gmail.com>
2017-03-22 15:47:22 -0500

    model concert

--- app/Concert.php
+++ app/Concert.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Concert extends Model
 {
-    //
+    protected $guarded = [];
+    protected $dates = ['date'];
+
+    public function getFormattedDateAttribute()
+    {
+        return $this->date->format('F j, Y');
+    }
+
+    public function getFormattedStartTimeAttribute()
+    {
+        return $this->date->format('g:ia');
+    }
 }
