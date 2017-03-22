28c926fc49937ab2b0f2b5927b5a162bbc3d9101
juan <chavezvasquezjuan@gmail.com>
2017-03-22 15:46:02 -0500

    database

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,5 +1,7 @@
 <?php
 
+use Carbon\Carbon;
+
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -20,5 +22,21 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Concert::class, function (Faker\Generator $faker) {
+
+    return [
+        'title' => 'The Red ChordDDD',
+        'subtitle' => 'with Animosity and Lethardgy',
+        'date' => Carbon::parse('+2 weeks'),
+        'ticket_price' => 2000,
+        'venue' => 'the mosh pit',
+        'venue_address' => '123 Example Lane',
+        'city' => 'Laraville',
+        'state' => 'ON',
+        'zip' => '17916',
+        'additional_information' => 'For tickets, call (555) 555-5555',
     ];
 });
